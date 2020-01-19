cc4880c99ee12f52af8b8a7cb1909f928a0fd9f7
GJ <GJ>
2020-01-19 23:00:35 +0800

    + chapter 10

---
+++ c10.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class c10 {
+public:
+	void test() {
+		vector<double> vd{ 1, 2, 3 };
+		auto sum = accumulate(vd.begin(), vd.end(), 0);
+
+		vector<string> vc{ "1", "2", "3" };
+		// auto str = accumulate(vc.begin(), vc.end(), ""); // const char* 没有定义+运算符
+
+		vector<int> vi;
+		fill_n(vi.begin(), 10, 0); // UB ERROR
+		fill_n(back_inserter(vi), 10, 0);
+
+		// copy
+		// replace replace_copy
+		// unique
+		// stable_sort
+		// find_if for_each
+
+		// lambda 捕获列表只用于局部非static变量
+		// bind placeholders
+
+	}
+};
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "c06.h"
 #include "c07.h"
 #include "c09.h"
+#include "c10.h"
 
 int main() {
     c03::test();
